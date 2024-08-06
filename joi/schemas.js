30ef21be214a8090b9d7b428400e4d95491d8315
unknown <eric.coimbra@gmail.com>
2024-08-06 20:07:20 +0100

    mvp complete

--- joi/schemas.js
+++ joi/schemas.js
@@ -1,9 +1,12 @@
 const Joi = require('joi');
 
+const todoState = Joi.string().valid('INCOMPLETE', 'COMPLETE').example('COMPLETE').description('Task state.').label('Task state');
+const todoDescription = Joi.string().min(1).example('Buy milk at the store.').description('Task description.');
+
 const todo = Joi.object({
   id: Joi.string().required().min(36).max(36).example('70a134cd-66a3-42ea-8f60-cc2202ec2c71').description('Unique identifier for Task entry.'),
-  description: Joi.string().min(1).required().example('Buy milk at the store.').description('Task description.'),
-  state: Joi.string().required().valid('INCOMPLETE', 'COMPLETE').example('COMPLETE').description('Task state.').label('Task state'),
+  description: todoDescription.required(),
+  state: todoState.required(),
   createdAt: Joi.date().required().example('2021-05-12T07:23:45.678Z').description('Date when the task was created.'),
   completedAt: Joi.date().allow(null).required().example('2021-05-13T11:23:45.678Z').description('Date when the task was completed. Can be null.'),
 }).label('Task object');
@@ -21,47 +24,35 @@ const todoGet = {
       .example('COMPLETED_AT')
       .description('Optional property to order the tasks in. Ascending. If null defaults to COMPLETED_AT.'),
   }).label('Todo query options'),
-  response: {
-    success: Joi.object({
-      todos: Joi.array().items(todo).label('Array of Tasks'),
-    }).label('Get tasks success response'),
-    failure: Joi.object({
-      message: Joi.string().required().example('Internal Server Error').description('Message with failure reason.'),
-    }).label('Get tasks failure response'),
-  },
+  response: Joi.object({
+    todos: Joi.array().items(todo).label('Array of Tasks'),
+  }).label('Get tasks success response'),
 };
 
 const todoPost = {
   request: Joi.object({
     description: Joi.string().min(1).required().example('Buy milk at the store.').description('Task description.'),
   }).label('Task creation payload'),
-  response: {
-    success: Joi.object({
-      newTodo: todo,
-    }).label('Task creation success response'),
-    failure: Joi.object({
-      message: Joi.string().required().example('Internal Server Error').description('Message with failure reason.'),
-    }).label('Task creation failure response'),
-  },
+  response: Joi.object({
+    newTodo: todo,
+  }).label('Task creation success response'),
 };
 
 const todoDel = {
   parameters: Joi.object({
     id: Joi.string().required().min(36).max(36).example('db28b3f7-13c2-4333-9c13-e6bb1bc5d107').description('Unique identifier of a task to delete.'),
   }),
-  response: {
-    success: undefined,
-    notFound: Joi.object({
-      message: Joi.string()
-        .min(1)
-        .required()
-        .example('Task not found.')
-        .description("If the id passed in the paremeters doens't match any task in the database."),
-    }).label('Not found response'),
-    failure: Joi.object({
-      message: Joi.string().required().example('Internal Server Error').description('Message with failure reason.'),
-    }).label('Task deletion failure response'),
-  },
+  success: undefined,
 };
 
-module.exports = { todoPost, todoGet, todoDel };
+const todoPatch = {
+  body: Joi.object({
+    state: todoState,
+    description: todoDescription,
+  })
+    .or('state', 'description')
+    .label('Task Patch request body'),
+  response: todo,
+};
+
+module.exports = { todoPost, todoGet, todoDel, todoPatch };
--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,6 +1,7 @@
-const { todoPost, todoGet, todoDel } = require('../joi/schemas');
+const { todoPost, todoGet, todoDel, todoPatch } = require('../joi/schemas');
 const config = require('../db/config');
 const knex = require('knex')(config.development);
+const Boom = require('@hapi/boom');
 
 const todoRoutes = [
   {
@@ -12,12 +13,18 @@ const todoRoutes = [
       tags: ['api', 'todos'],
       validate: {
         payload: todoPost.request,
+        failAction: (request, h, error) => {
+          console.error('Payload validation failed:', error.message);
+          throw Boom.badRequest(`Invalid request payload: ${error.message}`);
+        },
       },
       response: {
-        failAction: 'log',
+        failAction: (request, h, error) => {
+          console.error('Response validation failed:', error.message);
+          throw error;
+        },
         status: {
-          201: todoPost.response.success,
-          500: todoPost.response.failure,
+          201: todoPost.response,
         },
       },
     },
@@ -30,7 +37,7 @@ const todoRoutes = [
         return h.response({ newTodo: newTodo[0] }).code(201);
       } catch (error) {
         console.log(error);
-        return h.response({ message: 'Internal Server Error' + error.message }).code(505);
+        throw Boom.internal('Internal server erroror');
       }
     },
   },
@@ -43,12 +50,18 @@ const todoRoutes = [
       tags: ['api', 'todos'],
       validate: {
         query: todoGet.query,
+        failAction: (request, h, error) => {
+          console.error('Query validation failed:', error.message);
+          throw Boom.badRequest(`Invalid request query: ${error.message}`);
+        },
       },
       response: {
-        failAction: 'log',
+        failAction: (request, h, error) => {
+          console.error('Response validation failed:', error.message);
+          throw error;
+        },
         status: {
-          200: todoGet.response.success,
-          500: todoGet.response.failure,
+          200: todoGet.response,
         },
       },
     },
@@ -69,7 +82,7 @@ const todoRoutes = [
         return h.response({ todos }).code(200);
       } catch (error) {
         console.log(error);
-        return h.response({ message: 'Internal Server Error' + error.message }).code(505);
+        throw Boom.internal('Internal server erroror');
       }
     },
   },
@@ -82,13 +95,18 @@ const todoRoutes = [
       tags: ['api', 'todo'],
       validate: {
         params: todoDel.parameters,
+        failAction: (request, h, error) => {
+          console.error('Parameters validation failed:', error.message);
+          throw Boom.badRequest(`Invalid request parameters: ${error.message}`);
+        },
       },
       response: {
-        failAction: 'log',
+        failAction: (request, h, error) => {
+          console.error('Response validation failed:', error.message);
+          throw error;
+        },
         status: {
-          204: todoDel.response.success,
-          404: todoDel.response.notFound,
-          500: todoDel.response.failure,
+          204: todoDel.success,
         },
       },
     },
@@ -100,11 +118,65 @@ const todoRoutes = [
           await knex('ToDo').where('id', id).first().del();
           return h.response().code(204);
         } else {
-          return h.response({ message: 'Task not found' }).code(404);
+          return Boom.notFound('Task not found');
         }
       } catch (error) {
         console.log(error);
-        return h.response({ message: 'Internal Server Error' + error.message }).code(500);
+        throw Boom.internal('Internal server erroror');
+      }
+    },
+  },
+  {
+    method: 'PATCH',
+    path: '/todo/{id}',
+    options: {
+      description: 'Edits an item on the to-do list.',
+      notes: 'The edited item will be referenced by id using the URL parameter id.',
+      tags: ['api', 'todo'],
+      validate: {
+        payload: todoPatch.body,
+        failAction: (request, h, error) => {
+          console.error('Payload validation failed:', error.message);
+          throw Boom.badRequest(`Invalid request payload: ${error.message}`);
+        },
+      },
+      response: {
+        failAction: (request, h, error) => {
+          console.error('Response validation failed:', error.message);
+          throw error;
+        },
+        status: {
+          202: todoPatch.response,
+        },
+      },
+    },
+
+    handler: async (request, h) => {
+      const { id } = request.params;
+      const { payload } = request;
+      try {
+        const taskExists = await knex('ToDo').where('id', id).first();
+        if (taskExists) {
+          if (taskExists.state === 'COMPLETE' && payload.description) {
+            return Boom.badRequest('Task is already complete');
+          }
+          const [updatedTask] = await knex('ToDo')
+            .where('id', id)
+            .first()
+            .update({ ...payload, completedAt: payload.state === 'COMPLETE' ? knex.fn.now() : null }, [
+              'id',
+              'state',
+              'description',
+              'createdAt',
+              'completedAt',
+            ]);
+          return h.response({ updatedTask }).code(202);
+        } else {
+          return Boom.notFound('Task not found.');
+        }
+      } catch (error) {
+        console.log(error);
+        throw Boom.internal('Internal server error');
       }
     },
   },
